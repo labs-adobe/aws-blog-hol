e24d49345afbce531ef3bcd8478320f624250f0b
Navdeep Singh <navdeeps@adobe.com>
2024-08-04 17:39:31 +0530

    add code for bootcamp

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# event
-Adobe Event
+# Edge Hands-on Lab
+
+## Introduction
+This repo is a copy of what powers the AEM Edge website.
+This is trimmed and solely for the purpose of bootcamps and hands-on labs. 
---
+++ blocks/cards/cards.js
@@ -0,0 +1,37 @@
+export default function decorate(block) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('enter');
+      }
+    });
+  });
+
+  const classes = ['one', 'two', 'three', 'four', 'five'];
+  const row = block.children[0];
+  if (row) {
+    block.classList.add(classes[row.children.length - 1]);
+  }
+  block.querySelectorAll(':scope > div > div').forEach((cell) => {
+    if (cell.firstChild) {
+      const details = document.createElement('div');
+      details.classList.add('cards-card-details');
+      cell.classList.add('cards-card');
+      while (cell.firstChild) details.append(cell.firstChild);
+      const picture = details.querySelector('picture');
+      if (picture) {
+        cell.prepend(picture);
+      } else if (details.querySelector('h3')) {
+        cell.classList.add('cards-card-highlight');
+      }
+      // highlight styling
+      const links = details.querySelectorAll('a');
+      links.forEach((link) => {
+        link.classList.add('link-highlight-colorful-effect-hover-wrapper');
+        link.innerHTML = `<span class="link-highlight-colorful-effect-2">${link.textContent}</span>`;
+      });
+      cell.append(details);
+      observer.observe(cell);
+    }
+  });
+}
