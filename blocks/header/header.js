0250504df43c867bcce61f574d347282e7ca5dd5
Navdeep Singh <navdeeps@adobe.com>
2025-07-24 22:57:45 +0530

    update header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,7 +5,7 @@ import createTag from '../../utils/tag.js';
 import { changeTag, returnLinkTarget } from '../../utils/helpers.js';
 
 const ICON_ROOT = '/img';
-const BRAND_LOGO = '<img loading="lazy" alt="Adobe" width="27" height="27" src="/blocks/header/adobe-franklin-logo.svg">';
+const BRAND_LOGO = '<img loading="lazy" alt="Adobe" width="27" height="27" src="/blocks/header/aws-logo.png">';
 const SEARCH_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false">
 <path d="M14 2A8 8 0 0 0 7.4 14.5L2.4 19.4a1.5 1.5 0 0 0 2.1 2.1L9.5
 16.6A8 8 0 1 0 14 2Zm0 14.1A6.1 6.1 0 1 1 20.1 10 6.1 6.1 0 0 1 14 16.1Z">
